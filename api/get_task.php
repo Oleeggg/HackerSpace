074a200e191d14a3a3bccbc0bc33a81ab7c851f3
Oleeggg <shuvalova43@gmail.com>
2025-05-31 14:32:45 +0300

    commit

--- api/get_task.php
+++ api/get_task.php
@@ -226,7 +226,7 @@ function makeApiRequest(array $data): array {
         CURLOPT_HTTPHEADER => $headers,
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_SSL_VERIFYPEER => true,
-        CURLOPT_TIMEOUT => 30,
+        CURLOPT_TIMEOUT => 60,
         CURLOPT_HEADER => false
     ]);
 
